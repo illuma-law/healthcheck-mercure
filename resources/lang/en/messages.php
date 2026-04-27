a05d40ed626e94cc5b577b6db1e520eeebb81d0b
miguelenes <1568086+miguelenes@users.noreply.github.com>
2026-04-27 01:00:56 +0000

    Fix styling

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'installed'     => 'mercure extension is installed (version :version).',
-    'missing'       => 'mercure extension is required but not installed.',
+    'installed' => 'mercure extension is installed (version :version).',
+    'missing' => 'mercure extension is required but not installed.',
     'not_installed' => 'mercure extension is not installed.',
-    'query_failed'  => 'Could not verify mercure extension: :message',
+    'query_failed' => 'Could not verify mercure extension: :message',
 ];
